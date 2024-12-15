386152d9a36347b96e3b73a6f78782103546dec0
Justin McDonald <jmcdmoreno19@gmail.com>
2024-12-15 03:44:35 -0600

    add functionality for closing modal with escape key and clicking outside

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,31 @@
 import "./Modal.css"
 import closeButton from '../../assets/close.svg'
+import { useEffect } from "react"
 
 export default function Modal({children, isOpen, closeModal}){
+
+  useEffect(() => {
+    function handleEscapeClose(e) {
+      if (e.key === 'Escape' && isOpen) {
+        closeModal();
+      }
+    }
+
+    function handleOutsideClick(e) {
+      if (isOpen && e.target.classList.contains('modal')) {
+        closeModal();
+      }
+    }
+
+    window.addEventListener("keydown", handleEscapeClose);
+    window.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      window.removeEventListener("keydown", handleEscapeClose);
+      window.removeEventListener("click", handleOutsideClick);
+    };
+  }, [isOpen, closeModal]);
+
   return (
     <main className={`modal ${isOpen && "modal_visible"}`}>
       <section className="modal__container">
